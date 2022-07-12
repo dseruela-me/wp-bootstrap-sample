3382bda4a44bf38678c7a94aa2155571a2bfa0f5
Dan Seruela <freelancer.upwork.87@gmail.com>
2022-07-12 10:17:08 +0800

    Add custom classes to footer navigation links

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,7 +2,8 @@
 
 // Enable Navitation Menu
 register_nav_menus(array(
-    'primary'  =>  __('Primary Menu')
+    'primary'   =>  __('Primary Menu'),
+    'secondary' =>  __('Footer Menu'),
 ));
 
 // Adding classes to navigation li also selecting nav link for current page.
@@ -11,8 +12,12 @@ function add_additional_class_on_li($classes, $item, $args) {
         $classes[] = 'active';
     }
 
-    if(isset($args->add_li_class)) {
+    if(isset($args->add_li_class) && $args->theme_location == 'primary') {
         $classes[] = $args->add_li_class;
+    }
+
+    if(isset($args->add_footer_li_class) && $args->theme_location == 'secondary') {
+        $classes[] = $args->add_footer_li_class;
     }
 
     return $classes;
@@ -20,12 +25,19 @@ function add_additional_class_on_li($classes, $item, $args) {
 add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
 
 // Adding classes to navigation anchor tag.
-function add_links_atts($atts) {
-    $atts['class'] = 'nav-link';
+function add_links_atts($atts, $menu_item, $args) {
+    if ( $args->theme_location == 'primary' ) {
+        $atts['class'] = 'nav-link';
+    }
+
+    if ( $args->theme_location == 'secondary' ) {
+        $atts['class'] = 'nav-link p-0 text-muted';
+    }
+    
 
     return $atts;
 }
-add_filter('nav_menu_link_attributes', 'add_links_atts');
+add_filter('nav_menu_link_attributes', 'add_links_atts', null, 3);
 
 
 ?>
